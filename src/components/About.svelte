b2da7d628f3e9cc2e0d8b86463905c21099d2574
LucasGobbs <llcostagobbi@gmail.com>
2021-08-24 08:31:37 -0300

    Updates

--- src/components/About.svelte
+++
@@ -1,9 +0,0 @@
-<!-- <script>
-
-</script>
-
-<div>
-  <h3>This is About Page.</h3>
-  <Link to="/">Home</Link><br />
-  <Link to="blog">Blog</Link>
-</div> -->
--- src/components/Home.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-  let isOpen = false;
-
-  function handleUpdate(event) {
-    isOpen = event.detail.isOpen;
-  }
-</script>
-
-<p>Dentro da home</p>
